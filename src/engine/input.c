9db4e4d320226e2b43f4339fbbddb6129933afcf
PeepoClown <dvasev09@gmail.com>
2020-11-08 11:40:44 +0300

    input normed

--- src/engine/input.c
+++ src/engine/input.c
@@ -6,7 +6,7 @@
 /*   By: wupdegra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 18:01:56 by wupdegra          #+#    #+#             */
-/*   Updated: 2020/11/08 10:47:45 by wupdegra         ###   ########.fr       */
+/*   Updated: 2020/11/08 11:37:34 by wupdegra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,26 @@ static	char	*add_to_str(char *str, char c)
 	return (res);
 }
 
+static	void	input_loop(char ch, int read_res, char **input)
+{
+	if (!g_input_start)
+	{
+		free(*input);
+		*input = ft_strdup("");
+		g_input_start = true;
+	}
+	if (**input == '\0' && read_res == 0)
+	{
+		ft_putstr_fd("exit\n", 1);
+		free(*input);
+		exit(g_status);
+	}
+	else if (read_res == 0)
+		ft_putstr_fd("  \b\b", 1);
+	else
+		*input = add_to_str(*input, ch);
+}
+
 char			*user_input(void)
 {
 	char	*input;
@@ -43,22 +63,7 @@ char			*user_input(void)
 	{
 		if (buff[0] == '\n')
 			break ;
-		if (!g_input_start)
-		{
-			free(input);
-			input = ft_strdup("");
-			g_input_start = true;
-		}
-		if (*input == '\0' && read_res == 0)
-		{
-			ft_putstr_fd("exit\n", 1);
-			free(input);
-			exit(g_status);
-		}
-		else if (read_res == 0)
-			ft_putstr_fd("  \b\b", 1);
-		else
-			input = add_to_str(input, buff[0]);
+		input_loop(buff[0], read_res, &input);
 	}
 	if (read_res == -1)
 		exit (g_status);
